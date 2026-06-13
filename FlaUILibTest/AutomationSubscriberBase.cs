78bdc06a0f1478fcdc0f488045c551753e536f72
P D <pickslayer17@gmail.com>
2026-06-13 23:57:37 +0300

    AutomationSubscriberBase

---
+++ FlaUILibTest/AutomationSubscriberBase.cs
@@ -0,0 +1,50 @@
+﻿using FlaUI.Core.AutomationElements;
+using FlaUI.Core.Conditions;
+using FlaUILibTest.Interfaces;
+
+namespace FlaUILibTest;
+
+public abstract class AutomationSubscriberBase : ISubscriber
+{
+    private AutomationElement? _cached;
+    private TaskCompletionSource<AutomationElement>? _pendingRequest;
+    private readonly object _lock = new();
+
+    public ConditionBase SelfCondition { get; init; }
+    private readonly Module _module;
+
+    public AutomationSubscriberBase(Module module, ConditionBase condition)
+    {
+        _module = module;
+        SelfCondition = condition;
+        _module.AddSubscriber(this);
+    }
+
+    public void Update(AutomationElement? element)
+    {
+        lock (_lock)
+        {
+            _cached = element;
+            if (element != null && _pendingRequest != null)
+            {
+                _pendingRequest.TrySetResult(element);
+                _pendingRequest = null;
+            }
+        }
+    }
+
+    protected async Task<AutomationElement> GetElement()
+    {
+        lock (_lock)
+        {
+            if (_module.State == ModuleState.NotInitialized)
+                _module.TryInitialize();
+            if (_cached != null && _module.State == ModuleState.Ready)
+                return _cached;
+
+            _pendingRequest ??= new TaskCompletionSource<AutomationElement>(TaskCreationOptions.RunContinuationsAsynchronously);
+        }
+
+        return await _pendingRequest.Task;
+    }
+}
--- FlaUILibTest/Element.cs
+++ FlaUILibTest/Element.cs
@@ -1,70 +1,18 @@
 ﻿using FlaUI.Core.AutomationElements;
 using FlaUI.Core.Conditions;
-using FlaUILibTest.Interfaces;
 
 namespace FlaUILibTest;
 
-public class Element : ISubscriber
+public class Element : AutomationSubscriberBase
 {
-    public static int Timeout { get; set; } = 10_000;
-
-    private int SelfTimeout { get; set; } = 0;
-    private readonly Module _module;
-    private AutomationElement? _cached;
-    private TaskCompletionSource<AutomationElement>? _waiter;
-    private readonly object _lock = new();
-
-    public ConditionBase SelfCondition { get; }
-
-    public Element(Module module, ConditionBase condition)
+    public Element(Module module, ConditionBase condition) : base(module, condition)
     {
-        _module = module;
-        SelfCondition = condition;
-        _module.AddSubscriber(this);
-    }
-
-    public void Update(AutomationElement? element)
-    {
-        lock (_lock)
-        {
-            _cached = element;
-
-            if (element != null && _waiter != null)
-            {
-                _waiter.TrySetResult(element);
-                _waiter = null;
-            }
-        }
     }
 
     public Task<string> GetNameAsync() => WithElement(el => el.Properties.Name.ValueOrDefault ?? "");
     public Task<string> GetValueAsync() => WithElement(el => el.Patterns.Value.Pattern.Value?.ToString());
     public Task ClickAsync() => WithElement(el => el.Click());
 
-    private async Task<T> WithElement<T>(Func<AutomationElement, T> action)
-    {
-        var el = await GetElement();
-        return action(el);
-    }
-
-    private async Task WithElement(Action<AutomationElement> action)
-    {
-        var el = await GetElement();
-        action(el);
-    }
-
-    private async Task<AutomationElement> GetElement()
-    {
-        lock (_lock)
-        {
-            if (_module.State == ModuleState.NotInitialized)
-                _module.TryInitialize();
-            if (_cached != null && _module.State == ModuleState.Ready)
-                return _cached;
-
-            _waiter ??= new TaskCompletionSource<AutomationElement>(TaskCreationOptions.RunContinuationsAsynchronously);
-        }
-
-        return await _waiter.Task;
-    }
+    private async Task<T> WithElement<T>(Func<AutomationElement, T> action) => action(await GetElement());
+    private async Task WithElement(Action<AutomationElement> action) => action(await GetElement());
 }
