c1a35815e6469d884a0de54e0d34c6e1d826f84e
P D <pickslayer17@gmail.com>
2026-06-25 23:38:45 +0300

    big troubles, old engine works immediately, new thinking for whole lifetime

--- FlaUILibTest/Program.cs
+++ FlaUILibTest/Program.cs
@@ -6,47 +6,48 @@ class Program
 {
     static async Task Main()
     {
+        //var processStartInfo = new ProcessStartInfo(@"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE", "/e")
+        //{
+        //    WindowStyle = ProcessWindowStyle.Normal
+        //};
+        //processStartInfo.EnvironmentVariables["WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS"] = "--remote-debugging-port=9234";
+        //processStartInfo.UseShellExecute = false;
+        //var driver = new UIDriver.Driver();
+        //LogEventHandler.Subscribe(new ConsoleLogger());
+        //driver.Launch(processStartInfo);
+        //var elementFromDrvier = driver.Locator(cf => cf.ByControlType(ControlType.DataItem).And(cf.ByAutomationId("A1")));
+        //await elementFromDrvier.ClickAsync();
+        //Console.WriteLine("works!");
+
+        //Console.ReadLine();
+       await OleEngineTest();
+    }
+
+    public async static Task OleEngineTest()
+    {
         var processStartInfo = new ProcessStartInfo(@"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE", "/e")
         {
             WindowStyle = ProcessWindowStyle.Normal
         };
         processStartInfo.EnvironmentVariables["WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS"] = "--remote-debugging-port=9234";
         processStartInfo.UseShellExecute = false;
-        var driver = new UIDriver.Driver();
-        LogEventHandler.Subscribe(new ConsoleLogger());
-        driver.Launch(processStartInfo);
-        var elementFromDrvier = driver.Locator(cf => cf.ByControlType(ControlType.DataItem).And(cf.ByAutomationId("A1")));
-        await elementFromDrvier.ClickAsync();
+
+        //MonitorApp.Start();
+
+        var driver = new FlaUILibTest.UIDriver.UIDriver();
+        driver.LaunchApplication(processStartInfo);
+
+        var elementFromDrvier = driver.UILocator(cf => cf.ByControlType(ControlType.DataItem).And(cf.ByAutomationId("A1")));
         Console.WriteLine("works!");
+        await elementFromDrvier.ClickAsync();
 
-        Console.ReadLine();
+        var okButtonInFormatCells =
+           driver.UILocator(
+               cf => cf.ByControlType(ControlType.Button).And(cf.ByName("OK")),
+               cf => cf.ByControlType(ControlType.Window).And(cf.ByName("Format Cells"))
+               );
+        await okButtonInFormatCells.ClickAsync();
     }
-
-    //public async static void OleEngineTest()
-    //{
-    //    var processStartInfo = new ProcessStartInfo(@"C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE", "/e")
-    //    {
-    //        WindowStyle = ProcessWindowStyle.Normal
-    //    };
-    //    processStartInfo.EnvironmentVariables["WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS"] = "--remote-debugging-port=9234";
-    //    processStartInfo.UseShellExecute = false;
-
-    //    MonitorApp.Start();
-
-    //    var driver = new FlaUILibTest.UIDriver.UIDriver();
-    //    driver.LaunchApplication(processStartInfo);
-
-    //    var elementFromDrvier = driver.UILocator(cf => cf.ByControlType(ControlType.DataItem).And(cf.ByName("A1")));
-    //    Console.WriteLine("works!");
-    //    await elementFromDrvier.ClickAsync();
-
-    //    var okButtonInFormatCells =
-    //       driver.UILocator(
-    //           cf => cf.ByControlType(ControlType.Button).And(cf.ByName("OK")),
-    //           cf => cf.ByControlType(ControlType.Window).And(cf.ByName("Format Cells"))
-    //           );
-    //    await okButtonInFormatCells.ClickAsync();
-    //}
 
     //public async static void DcPushTest()
     //{
